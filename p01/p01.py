86517cd573bf9d5bb4b60570812a91e662edb26a
BeckTob <beckmann@uni-bonn.de>
2024-12-02 15:48:20 +0100

    feat(p01): solve demo part 1

--- p01/p01.py
+++ p01/p01.py
@@ -7,3 +7,7 @@ def distances_of_smallest(list_a: Iterable, list_b: Iterable):
     list_b.sort()
 
     return [abs(a-b) for a, b in zip(list_a, list_b)]
+
+def solve_part_1(raw_input: str):
+    list_a, list_b = zip(*(map(int, line.split()) for line in raw_input.splitlines()))
+    return sum(distances_of_smallest(list_a, list_b))
--- p01/test_p01.py
+++ p01/test_p01.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from p01.p01 import distances_of_smallest
+from p01.p01 import distances_of_smallest, solve_part_1
 
 demo_input = """3   4
 4   3
@@ -17,3 +17,6 @@ class Test(TestCase):
     def test_distances_of_smallest(self):
         distances = distances_of_smallest(demo_list_a, demo_list_b)
         self.assertEqual([2, 1, 0, 1, 2, 5], distances)
+
+    def test_solve_demo_1(self):
+        self.assertEqual(11, solve_part_1(demo_input))
