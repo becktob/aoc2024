8e6390289cf0d571532a1567200e893e3b273a68
BeckTob <beckmann@uni-bonn.de>
2024-12-02 09:39:28 +0100

    test: add more cases solved by existing code

--- p01/test_p01.py
+++ p01/test_p01.py
@@ -2,6 +2,14 @@ from unittest import TestCase
 
 from p01.p01 import report_is_good
 
+
 class Test01(TestCase):
     def test_report_is_good_demo(self):
-        self.assertTrue(report_is_good([7,6,4,2,1]))
+        demo_reports = (
+            ([7, 6, 4, 2, 1], True),
+            ([1, 2, 7, 8, 9], False),
+            ([9, 7, 6, 2, 1], False),
+        )
+        for report, is_good in demo_reports:
+            with self.subTest(report):
+                self.assertEqual(is_good, report_is_good(report))
