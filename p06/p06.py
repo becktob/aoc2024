5d8a43bfbab17d47ff4cd1c3e192552141025c3f
Tobias <beckmann@uni-bonn.de>
2024-12-06 10:28:46 +0100

    speed(p06): store next_position in loop, much less argwhere

--- p06/p06.py
+++ p06/p06.py
@@ -8,11 +8,14 @@ directions = {'^': (-1, 0),
               '<': (0, -1)}
 
 
-def one_step(maze):
-    current_direction = next(filter(lambda d: d in maze, directions.keys()))
-    current_position = numpy.argwhere(maze == current_direction)
-    assert 1 == len(current_position)
-    current_position = current_position[0]
+def one_step(maze, current_position=None):
+    if current_position is None:
+        current_direction = next(filter(lambda d: d in maze, directions.keys()))
+        current_position = numpy.argwhere(maze == current_direction)
+        assert 1 == len(current_position)
+        current_position = current_position[0]
+    else:
+        current_direction = maze[*current_position]
     maze[*current_position] = 'X'  # may be overwritten on turn
 
     next_position = current_position + directions[current_direction]
@@ -33,8 +36,8 @@ def one_step(maze):
 def solve(raw_input):
     maze = string_to_array(raw_input)
 
-    next_position = True
+    maze, next_position = one_step(maze, None)
     while next_position is not None:
-        maze, next_position = one_step(maze)
+        maze, next_position = one_step(maze, next_position)
 
     return len(numpy.argwhere(maze == 'X'))
