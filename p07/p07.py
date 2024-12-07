5d0d3d33ede457642bbba4eaf033c7787d4da09e
BeckTob <beckmann@uni-bonn.de>
2024-12-07 14:27:20 +0100

    feat(p07): test true-ability for more than two operands

--- p07/p07.py
+++ p07/p07.py
@@ -1,8 +1,20 @@
+import itertools
 import operator
 
 
 def can_make_true(result: int, operands: list[int]):
     possible_operators = (operator.add, operator.mul)
-    possible_results = [op(*operands)
-                        for op in possible_operators]
+
+    operator_combinations = itertools.product(
+        possible_operators,
+        repeat=len(operands) - 1)
+
+    possible_results = [apply(ops, operands) for ops in operator_combinations]
     return any(result == r for r in possible_results)
+
+
+def apply(ops, operands):
+    acc = operands[0]
+    for op, operand in zip(ops, operands[1:]):
+        acc = op(acc, operand)
+    return acc
--- p07/test_p07.py
+++ p07/test_p07.py
@@ -4,6 +4,10 @@ from p07.p07 import can_make_true
 
 
 class Test(TestCase):
-    def test_can_make_true(self):
+    def test_can_make_true_two(self):
         self.assertTrue(can_make_true(190, [10, 19]))
         self.assertFalse(can_make_true(83, [17, 5]))
+
+    def test_can_make_true_more(self):
+        self.assertTrue(can_make_true(3267, [81, 40, 27]))
+        self.assertTrue(can_make_true(292, [11, 6 ,16, 20]))
