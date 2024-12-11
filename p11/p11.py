0ae245ecd423d1e8a607d03aedbf6e4b7b56378f
BeckTob <beckmann@uni-bonn.de>
2024-12-11 07:56:19 +0100

    feat(p11): implement more rules

--- p11/p11.py
+++ p11/p11.py
@@ -5,5 +5,7 @@ def blink(stones: Iterable[int]) -> Iterator[int]:
     for stone in stones:
         if stone == 0:
             yield 1
+        elif 10 <= stone <= 99:
+            yield from (int(digit) for digit in str(stone))
         else:
-            raise NotImplementedError
+            yield stone * 2024
--- p11/test_p11.py
+++ p11/test_p11.py
@@ -7,3 +7,7 @@ class Test(TestCase):
     def test_blink_0_becomes_1(self):
         stones = list(blink([0]))
         self.assertListEqual([1], stones)
+
+    def test_demo_first_step(self):
+        stones = list(blink([125, 17]))
+        self.assertListEqual([253000, 1, 7], stones)
