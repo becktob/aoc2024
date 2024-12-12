7878409f33895e1387e9ea306850e849cc35c1c2
BeckTob <beckmann@uni-bonn.de>
2024-12-12 08:21:57 +0100

    feat(p12): find perimeters

--- p12/p12.py
+++ p12/p12.py
@@ -7,6 +7,20 @@ import numpy
 class Area:
     letter: str
     size: int
+    perimeter: int
+
+
+def find_perimeter(plots) -> int:
+    directions = ((0,1),  (0,-1), (1,0), (-1,0))
+
+    borders = 0
+    for plot in plots:
+        for d in directions:
+            if not any(all(plot +d == p) for p in plots):
+               borders += 1
+
+    return borders
+
 
 
 def find_areas(map: numpy.ndarray) -> list[Area]:
@@ -15,6 +29,6 @@ def find_areas(map: numpy.ndarray) -> list[Area]:
     areas = []
     for letter in letters:
         plots = numpy.argwhere(map == letter)
-        areas.append(Area(letter, len(plots)))
+        areas.append(Area(letter, len(plots), find_perimeter(plots)))
 
     return areas
--- p12/test_p12.py
+++ p12/test_p12.py
@@ -16,3 +16,9 @@ class Test(TestCase):
         areas = find_areas(map)
         sizes = [a.size for a in areas]
         self.assertListEqual([1, 3, 4, 4, 4], sorted(sizes))
+
+    def test_find_area_perimeters(self):
+        map = string_to_array(demo_input_12_1)
+        areas = find_areas(map)
+        perimeters = [a.perimeter for a in areas]
+        self.assertListEqual([4, 8, 8, 10, 10], sorted(perimeters))
