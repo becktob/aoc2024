a7e80b3ee32892971e6fb23702d72a41accd2455
BeckTob <beckmann@uni-bonn.de>
2024-12-13 06:50:44 +0100

    feat(p13): find solutions

--- p13/p13.py
+++ p13/p13.py
@@ -1,5 +1,7 @@
 import re
 
+import numpy
+
 
 def parse_xy(raw_button):
     m = re.match('.*: X=?(.*?), Y=?(.*)', raw_button)
@@ -13,3 +15,11 @@ class ClawMachine:
         self.xy_a = parse_xy(raw_a)
         self.xy_b = parse_xy(raw_b)
         self.xy_prize = parse_xy(raw_prize)
+
+    def solutions_ab(self):
+        for n_a in range(100):
+            loc_a = numpy.array(self.xy_a) * n_a
+            for n_b in range(100):
+                loc_b = numpy.array(self.xy_b) * n_b
+                if all(loc_b + loc_a == self.xy_prize):
+                    yield n_a, n_b
--- p13/test_p13.py
+++ p13/test_p13.py
@@ -13,3 +13,12 @@ Prize: X=8400, Y=5400"""
         self.assertEqual((94, 34), machine.xy_a)
         self.assertEqual((22, 67), machine.xy_b)
         self.assertEqual((8400, 5400), machine.xy_prize)
+
+    def test_solutions_ab(self):
+        raw_input = """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400"""
+        machine = ClawMachine(raw_input)
+
+        solutions = list(machine.solutions_ab())
+        self.assertIn((80, 40), solutions)
