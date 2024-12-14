a0556f5c06e8cc58ccac7e04c9a45238b07396ae
BeckTob <beckmann@uni-bonn.de>
2024-12-14 08:26:55 +0100

    feat(p14): solve first motion example

--- p14/p14.py
+++ p14/p14.py
@@ -6,3 +6,6 @@ class Robot:
         m = re.match("p=(.*) v=(.*)", raw_line)
         self.p_xy = tuple(int(xy) for xy in m.group(1).split(','))
         self.v_xy = tuple(int(xy) for xy in m.group(2).split(','))
+
+    def move(self, steps: int, room_size: tuple[int, int]):
+        self.p_xy = tuple((p+steps*v)%s for p,v,s in zip(self.p_xy, self.v_xy, room_size) )
--- p14/test_p14.py
+++ p14/test_p14.py
@@ -9,3 +9,8 @@ class TestRobot(TestCase):
         r = Robot(raw_input)
         self.assertEqual((0, 4), r.p_xy)
         self.assertEqual((3, -3), r.v_xy)
+
+    def test_move(self):
+        r = Robot('p=2,4 v=2,-3')
+        r.move(5, (11,7))
+        self.assertEqual((1,3),r.p_xy)
