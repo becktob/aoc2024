e67374d842ecc556b47fc6ca30c048cf88f9c9ce
BeckTob <beckmann@uni-bonn.de>
2024-12-14 08:20:40 +0100

    feat(p14): parse Robot

---
+++ p14/p14.py
@@ -0,0 +1,7 @@
+import re
+
+
+class Robot:
+    def __init__(self, raw_line: str):
+        m = re.match("p=(0,4) v=(3,-3)", raw_line)
+        self.p_xy = tuple(int(xy) for xy in m.group(1).split(','))
---
+++ p14/test_p14.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from p14.p14 import Robot
+
+
+class TestRobot(TestCase):
+    def test_parse(self):
+        raw_input = 'p=0,4 v=3,-3'
+        r = Robot(raw_input)
+        self.assertEqual((0, 4), r.p_xy)
