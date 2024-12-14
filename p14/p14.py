0d93ed3a051bc81517e59e6fc40b583f9b247224
BeckTob <beckmann@uni-bonn.de>
2024-12-14 08:52:30 +0100

    refactor(p14): extract .x and .y for readability

--- p14/p14.py
+++ p14/p14.py
@@ -15,6 +15,14 @@ class Robot:
     def move(self, steps: int, room_size: tuple[int, int]):
         self.p_xy = tuple((p + steps * v) % s for p, v, s in zip(self.p_xy, self.v_xy, room_size))
 
+    @property
+    def x(self):
+        return self.p_xy[0]
+
+    @property
+    def y(self):
+        return self.p_xy[1]
+
 
 def solve_part_1(raw_input: str, room_size=(101, 103)):
     steps = 100
@@ -24,9 +32,9 @@ def solve_part_1(raw_input: str, room_size=(101, 103)):
 
     x_half = room_size[0] // 2
     y_half = room_size[1] // 2
-    nw = [r for r in robots if 0 <= r.p_xy[0] < x_half and 0 < r.p_xy[1] < y_half]
-    ne = [r for r in robots if x_half < r.p_xy[0] and 0 <= r.p_xy[1] < y_half]
-    sw = [r for r in robots if 0 <= r.p_xy[0] < x_half and y_half < r.p_xy[1]]
-    se = [r for r in robots if x_half < r.p_xy[0] and y_half < r.p_xy[1]]
+    nw = [r for r in robots if 0 <= r.x < x_half and 0 < r.y < y_half]
+    ne = [r for r in robots if x_half < r.x and 0 <= r.y < y_half]
+    sw = [r for r in robots if 0 <= r.x < x_half and y_half < r.y]
+    se = [r for r in robots if x_half < r.x and y_half < r.y]
 
     return reduce(operator.mul, map(len, (nw, ne, sw, se)), 1)
