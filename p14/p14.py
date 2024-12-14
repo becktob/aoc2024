3307194df65bffc1722a3db149d5a6e1b4ae7245
BeckTob <beckmann@uni-bonn.de>
2024-12-14 08:26:33 +0100

    fix(p14): parse more than the first example robot -.-

--- p14/p14.py
+++ p14/p14.py
@@ -3,6 +3,6 @@ import re
 
 class Robot:
     def __init__(self, raw_line: str):
-        m = re.match("p=(0,4) v=(3,-3)", raw_line)
+        m = re.match("p=(.*) v=(.*)", raw_line)
         self.p_xy = tuple(int(xy) for xy in m.group(1).split(','))
         self.v_xy = tuple(int(xy) for xy in m.group(2).split(','))
