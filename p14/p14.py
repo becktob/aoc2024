ff00a86a408ccc500cbe9f6ba8f5174885b92f4b
BeckTob <beckmann@uni-bonn.de>
2024-12-14 08:41:36 +0100

    feat(p14): solve demo 1

--- p14/p14.py
+++ p14/p14.py
@@ -1,4 +1,6 @@
+import operator
 import re
+from functools import reduce
 
 
 class Robot:
@@ -7,5 +9,24 @@ class Robot:
         self.p_xy = tuple(int(xy) for xy in m.group(1).split(','))
         self.v_xy = tuple(int(xy) for xy in m.group(2).split(','))
 
+    def __repr__(self):
+        return f"<Robot at {self.p_xy}>"
+
     def move(self, steps: int, room_size: tuple[int, int]):
-        self.p_xy = tuple((p+steps*v)%s for p,v,s in zip(self.p_xy, self.v_xy, room_size) )
+        self.p_xy = tuple((p + steps * v) % s for p, v, s in zip(self.p_xy, self.v_xy, room_size))
+
+
+def solve_part_1(raw_input: str, room_size=(101, 103)):
+    steps = 100
+    robots = [Robot(r) for r in raw_input.splitlines()]
+
+    [r.move(steps, room_size) for r in robots]
+
+    x_half = room_size[0] // 2
+    y_half = room_size[1] // 2
+    nw = [r for r in robots if 0 <= r.p_xy[0] < x_half and 0 < r.p_xy[1] < y_half]
+    ne = [r for r in robots if x_half < r.p_xy[0] and 0 <= r.p_xy[1] < y_half]
+    sw = [r for r in robots if 0 <= r.p_xy[0] < x_half and y_half < r.p_xy[1]]
+    se = [r for r in robots if x_half < r.p_xy[0] and y_half < r.p_xy[1]]
+
+    return reduce(operator.mul, map(len, (nw, ne, sw, se)), 1)
--- p14/test_p14.py
+++ p14/test_p14.py
@@ -1,7 +1,20 @@
 from unittest import TestCase
 
-from p14.p14 import Robot
+from p14.p14 import Robot, solve_part_1
 
+demo_input_14 = """p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3
+"""
 
 class TestRobot(TestCase):
     def test_parse(self):
@@ -14,3 +27,6 @@ class TestRobot(TestCase):
         r = Robot('p=2,4 v=2,-3')
         r.move(5, (11,7))
         self.assertEqual((1,3),r.p_xy)
+
+    def test_solve_demo_1(self):
+        self.assertEqual(12, solve_part_1(demo_input_14, room_size=(11,7)))
