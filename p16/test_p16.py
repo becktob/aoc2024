f9c3f4d5ea7e8ecff1532d98f7d6379ca4f190e9
BeckTob <beckmann@uni-bonn.de>
2024-12-16 10:29:50 +0100

    test(p16): add second test case

--- p16/test_p16.py
+++ p16/test_p16.py
@@ -1,3 +1,4 @@
+import unittest
 from unittest import TestCase
 
 from p16.p16 import solve_part_1
@@ -18,7 +19,33 @@ demo_input_16_1 = """###############
 #S..#.....#...#
 ###############"""
 
+demo_input_16_2 = """#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################"""
+
 
 class TestMaze(TestCase):
     def test_solve_demo_1(self):
         self.assertEqual(7036, solve_part_1(demo_input_16_1))
+
+    def test_solve_demo_1_2(self):
+        self.assertEqual(11048, solve_part_1(demo_input_16_2))
+
+    @unittest.skip('slow')
+    def test_solve_part_1(self):
+        with open('p16/input') as f:
+            self.assertEqual(1, solve_part_1(f.read()))
