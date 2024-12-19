f6ca92c116318ec3bf62a5f610dd4ac1facebf32
BeckTob <beckmann@uni-bonn.de>
2024-12-19 08:39:46 +0100

    feat(p19): solve part 1 with faster can_combine()

--- p19/p19.py
+++ p19/p19.py
@@ -19,6 +19,19 @@ def find_combinations(design: str, towels: list[str]) -> list[list[str]]:
     return combinations
 
 
+def can_combine(design: str, towels: list[str]) -> bool:
+    towels_matching_beginning = [t for t in towels if all(d == t for d, t in zip(design, t))]
+
+    for beginning in towels_matching_beginning:
+        if beginning == design:
+            return True
+        elif remaining := design[len(beginning):]:
+            if can_combine(remaining, towels):
+                return True
+
+    return False
+
+
 def solve_part_1(raw_input: str):
     towels, designs = parse_towels_designs(raw_input)
-    return sum(1 for d in designs if find_combinations(d, towels))
+    return sum(1 for d in designs if can_combine(d, towels))
--- p19/test_p19.py
+++ p19/test_p19.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from p19.p19 import parse_towels_designs, find_combinations, solve_part_1
+from p19.p19 import parse_towels_designs, find_combinations, solve_part_1, can_combine
 
 demo_input_19 = """r, wr, b, g, bwu, rb, gb, br
 
@@ -29,5 +29,15 @@ class Test(TestCase):
         towels, designs = parse_towels_designs(demo_input_19)
         self.assertEqual([], find_combinations('ubwu', towels))
 
+    def test_can_combine(self):
+        towels, designs = parse_towels_designs(demo_input_19)
+        self.assertTrue(can_combine('brwrr', towels))
+        self.assertTrue(can_combine('bwurrg', towels))
+        self.assertFalse(can_combine('ubwu', towels))
+
     def test_solve_demo_1(self):
         self.assertEqual(6, solve_part_1(demo_input_19))
+
+    def test_solve_part1(self):
+        with open('p19/input') as f:
+            self.assertEqual(258, solve_part_1(f.read()))
