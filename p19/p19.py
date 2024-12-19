a2a9c08a345edd3e494489e6e7f8e976aec4a776
BeckTob <beckmann@uni-bonn.de>
2024-12-19 08:23:06 +0100

    feat(p19): parse, find combinations for first demo line

---
+++ p19/p19.py
@@ -0,0 +1,19 @@
+def parse_towels_designs(raw_input: str) -> (list[str], list[str]):
+    raw_towels, raw_designs = raw_input.split('\n\n')
+    towels = raw_towels.split(', ')
+    designs = raw_designs.splitlines()
+    return towels, designs
+
+
+def find_combinations(design: str, towels: list[str]) -> list[list[str]]:
+    towels_matching_beginning = [t for t in towels if all(d == t for d, t in zip(design, t))]
+
+    combinations = []
+    for beginning in towels_matching_beginning:
+        if beginning == design:
+            combinations.append([beginning])
+        elif remaining := design[len(beginning):]:
+            ends = find_combinations(remaining, towels)
+            combinations += [[beginning] + end for end in ends]
+
+    return combinations
---
+++ p19/test_p19.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+from p19.p19 import parse_towels_designs, find_combinations
+
+demo_input_19 = """r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"""
+
+
+class Test(TestCase):
+    def test_parse_towels_designs(self):
+        towels, designs = parse_towels_designs(demo_input_19)
+        self.assertEqual(8, len(towels))
+        self.assertEqual('wr', towels[1])
+        self.assertEqual('brgr', designs[-2])
+
+    def test_find_combination(self):
+        towels, designs = parse_towels_designs(demo_input_19)
+        self.assertIn(['br', 'wr', 'r'], find_combinations('brwrr', towels))
