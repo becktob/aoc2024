83ccc2e7ac1627af9ee8a4f1dfdb234caa9a2b14
BeckTob <beckmann@uni-bonn.de>
2024-12-22 11:54:34 +0100

    feat(p22): evolve often, solve demo part 1

--- p22/p22.py
+++ p22/p22.py
@@ -7,3 +7,14 @@ def evolve_secret_number(number: int) -> int:
     number = mix_prune(number, number // 32)
     number = mix_prune(number, number * 2048)
     return number
+
+def evolve_often(number:int, times:int) -> int:
+    for _ in range(times):
+        number = evolve_secret_number(number)
+    return number
+
+
+def solve_part_1(raw_input: str) -> int:
+    initial_secrets = [int(l) for l in raw_input.splitlines()]
+
+    return sum(evolve_often(number, 2000) for number in initial_secrets)
--- p22/test_p22.py
+++ p22/test_p22.py
@@ -1,9 +1,16 @@
 from unittest import TestCase
 
-from p22.p22 import evolve_secret_number
+from p22.p22 import evolve_secret_number, solve_part_1
 
+demo_input_22 ="""1
+10
+100
+2024"""
 
 class Test(TestCase):
     def test_evolve_secret_number(self):
         self.assertEqual(15887950, evolve_secret_number(123))
         self.assertEqual(16495136, evolve_secret_number(15887950))
+
+    def test_solve_demo_1(self):
+        self.assertEqual(37327623, solve_part_1(demo_input_22))
