aa777342d9113c82db5ef9315e8752014d6c21a0
BeckTob <beckmann@uni-bonn.de>
2024-12-24 20:35:34 +0100

    feat(p24): is_and() and swapped_device()

--- p24/p24.py
+++ p24/p24.py
@@ -1,4 +1,5 @@
 import operator
+from typing import Iterable
 
 
 class Gate:
@@ -34,10 +35,47 @@ class Device:
         input_values = [self.get_value(i) for i in gate.inputs]
         return gate.op(*input_values)
 
+    def get_z(self):
+        z_wires = [g for g in self.gates.keys() if g[0] == 'z']
+        return sum(2 ** int(wire[1:]) * self.get_value(wire) for wire in z_wires)
+
+    def set_x(self, val: int):
+        self.set_input(val, 'x')
+
+    def set_y(self, val: int):
+        self.set_input(val, 'y')
+
+    def set_input(self, val: int, xy: str):
+        wires = [g for g in self.inits.keys() if g[0] == xy]
+        for bit in range(len(wires)):
+            self.inits[f"{xy}{bit:02d}"] = bool(val & (2 ** bit))
+
 
 def solve_part_1(raw_input: str):
     device = Device(raw_input)
 
-    z_wires = [g for g in device.gates.keys() if g[0] == 'z']
+    return device.get_z()
 
-    return sum(2 ** int(wire[1:]) * device.get_value(wire) for wire in z_wires)
+
+def is_and(device: Device) -> bool:
+    input_bit_count = sum(1 for x in device.inits.keys() if x[0] == 'x')
+
+    for bit in range(input_bit_count):
+        for op1, op2 in (2 ** bit, 2 ** bit), (0, 2 ** bit), (2 ** bit, 0):
+            device.set_x(op1)
+            device.set_y(op2)
+
+            if device.get_z() != operator.and_(op1, op2):
+                return False
+
+    return True
+
+
+def swapped_device(raw_input: str, gates_to_swap: Iterable[tuple[str, str]]):
+    device = Device(raw_input)
+    for (a, b) in gates_to_swap:
+        swap_gates = {a: device.gates[b],
+                      b: device.gates[a]}
+        device.gates.update(swap_gates)
+
+    return device
--- p24/test_p24.py
+++ p24/test_p24.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from p24.p24 import Device, solve_part_1
+from p24.p24 import Device, solve_part_1, swapped_device, is_and
 
 demo_24_small = """x00: 1
 x01: 1
@@ -12,6 +12,27 @@ y02: 0
 x00 AND y00 -> z00
 x01 XOR y01 -> z01
 x02 OR y02 -> z02
+"""
+
+demo_24_add = """x00: 0
+x01: 1
+x02: 0
+x03: 1
+x04: 0
+x05: 1
+y00: 0
+y01: 0
+y02: 1
+y03: 1
+y04: 0
+y05: 1
+
+x00 AND y00 -> z05
+x01 AND y01 -> z02
+x02 AND y02 -> z01
+x03 AND y03 -> z03
+x04 AND y04 -> z04
+x05 AND y05 -> z00
 """
 
 
@@ -36,3 +57,11 @@ class TestDevice(TestCase):
     def test_solve_part_1(self):
         with open('p24/input') as f:
             self.assertEqual(69201640933606, solve_part_1(f.read()))
+
+    def test_is_and(self):
+        device = swapped_device(demo_24_add, ())
+        self.assertFalse(is_and(device))
+
+    def test_is_and_repaired(self):
+        repaired_device = swapped_device(demo_24_add, (('z00', 'z05'), ('z02', 'z01')))
+        self.assertTrue(is_and(repaired_device))
